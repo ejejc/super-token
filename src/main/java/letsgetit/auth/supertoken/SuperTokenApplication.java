86d5a1d97414374c2d906f99e83f30b5f4441479
ansehyeong <dkstpgud@gmail.com>
2022-03-29 20:03:54 +0900

    initial commit

---
+++ src/main/java/letsgetit/auth/supertoken/SuperTokenApplication.java
@@ -0,0 +1,13 @@
+package letsgetit.auth.supertoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/letsgetit/auth/supertoken/SuperTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package letsgetit.auth.supertoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
