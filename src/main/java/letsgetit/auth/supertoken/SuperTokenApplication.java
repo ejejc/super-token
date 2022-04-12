26ef1e07e6b9e8df574ff6fca811b0c2689a8370
ansehyeong <dkstpgud@gmail.com>
2022-04-12 20:35:55 +0900

    feat: properties mapping pojo class 생성
    - configuration properties 사용을 위한 어노테이션 추가
    - application.yml 프로젝트 설정 추가
    - properties mapping 을 위한 pojo class 생성

--- src/main/java/letsgetit/auth/supertoken/SuperTokenApplication.java
+++ src/main/java/letsgetit/auth/supertoken/SuperTokenApplication.java
@@ -1,8 +1,11 @@
 package letsgetit.auth.supertoken;
 
+import letsgetit.auth.supertoken.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class SuperTokenApplication {
 
