3fad8e9ff82b3d422feef60e2f7e6c13ca27da03
ansehyeong <dkstpgud@gmail.com>
2022-04-17 15:10:38 +0900

    #anpopo feat: validation library 추가 / domain package 추가

---
+++ src/main/java/letsgetit/auth/supertoken/domain/AuthProvider.java
@@ -0,0 +1,8 @@
+package letsgetit.auth.supertoken.domain;
+
+public enum AuthProvider {
+    LOCAL,
+    GOOGLE,
+    FACEBOOK,
+    GITHUB
+}
---
+++ src/main/java/letsgetit/auth/supertoken/domain/User.java
@@ -0,0 +1,40 @@
+package letsgetit.auth.supertoken.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = @UniqueConstraint(columnNames = "email")
+)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean emailVerified = false;
+
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    private String providerId;
+
+}
