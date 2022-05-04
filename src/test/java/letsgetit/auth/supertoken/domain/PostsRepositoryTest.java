9487db6cc4d6d6b03d2a00277a15e74c4c08e11a
ansehyeong <dkstpgud@gmail.com>
2022-05-04 19:29:58 +0900

    feat: domain 생성 및 테스트 코드 작성

---
+++ src/test/java/letsgetit/auth/supertoken/domain/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package letsgetit.auth.supertoken.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("dkstpgud@gmail.com")
+                        .build()
+        );
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertEquals(posts.getTitle(), title);
+        assertEquals(posts.getTitle(), title);
+    }
+
+}
