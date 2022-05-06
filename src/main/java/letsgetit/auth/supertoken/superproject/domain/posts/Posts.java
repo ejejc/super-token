d3e3d0f26d548247ae481d4b571b55a52acac99a
최은지 <ejejc@naver.com>
2022-05-06 10:01:35 +0900

    은지: 슈퍼 프로젝트 스터디.

---
+++ src/main/java/letsgetit/auth/supertoken/superproject/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package letsgetit.auth.supertoken.superproject.domain.posts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity
+@Getter
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    // 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/letsgetit/auth/supertoken/superproject/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package letsgetit.auth.supertoken.superproject.domain.posts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    // 단위 테스트가 끝날때 마다 수행되는 메소드
+    @AfterEach
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+        .title(title)
+        .content(content)
+        .author("ejejc@naver.com").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
