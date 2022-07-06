421ec6a53b179c50fbd4418f0fb9e1acac5857c5
eunjis <ejejc@naver.com>
2022-07-06 21:58:40 +0900

    eunji: naver login 연동.

--- src/main/java/letsgetit/auth/supertoken/config/SecurityConfig.java
+++ src/main/java/letsgetit/auth/supertoken/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+    @Override  
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
